d35a64afb66476ed5db8431c529cea0e58b3231f
Sergei Kovalchuk <sergei.kovalchuk@arsysop.ru>
2020-04-29 18:28:59 +0300

    Added implementation for viewer

---
+++ bundles/org.eclipse.chronograph.viewer/src/org/eclipse/chronograph/viewer/ChronographViewer.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ *	Copyright (c) 2020 ArSysOp
+ *
+ *	This program and the accompanying materials are made available under the
+ *	terms of the Eclipse Public License 2.0 which is available at
+ *	http://www.eclipse.org/legal/epl-2.0.
+ *
+ *	SPDX-License-Identifier: EPL-2.0
+ *
+ *	Contributors:
+ *	Sergei Kovalchuk <sergei.kovalchuk@arsysop.ru> - 
+ *												initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chronograph.viewer;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.chronograph.internal.api.Styler;
+import org.eclipse.chronograph.internal.api.providers.ContainerProvider;
+import org.eclipse.chronograph.internal.base.UnitConverter;
+import org.eclipse.chronograph.internal.swt.BrickStyler;
+import org.eclipse.chronograph.internal.swt.GroupStyler;
+import org.eclipse.chronograph.internal.swt.RulerStyler;
+import org.eclipse.chronograph.internal.swt.SectionStyler;
+import org.eclipse.chronograph.internal.swt.StageStyler;
+import org.eclipse.chronograph.internal.swt.stage.ChronographStage;
+import org.eclipse.chronograph.swt.internal.stage.ChronographStageImpl;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+
+public class ChronographViewer {
+	private final ChronographStage stage;
+	private final List<Styler> stylers;
+
+	public ChronographViewer(Composite parent, ContainerProvider provider) {
+		this.stage = new ChronographStageImpl(parent, SWT.BORDER, provider);
+		this.stage.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		this.stage.navigateToUnit(UnitConverter.localDatetoUnits(LocalDate.now().minusDays(7)));
+		stylers = new ArrayList<>();
+		stylers.add(new BrickStyler());
+		stylers.add(new StageStyler());
+		stylers.add(new GroupStyler());
+		stylers.add(new SectionStyler());
+		stylers.add(new RulerStyler());
+		initClassicStyle();
+	}
+
+	private void initClassicStyle() {
+		stylers.stream().forEach(Styler::initClassicTheme);
+	}
+
+	public void setProvider(ContainerProvider provider) {
+		this.stage.setProvider(provider);
+	}
+}
