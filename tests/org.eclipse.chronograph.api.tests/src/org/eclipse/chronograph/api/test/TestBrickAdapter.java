06889e6684a2454a32a4f9af93e25a60384bb3ef
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2020-05-16 22:12:30 +0300

    #28 Adapt Chronograph for Passage Operator
    Removed unused builders
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- tests/org.eclipse.chronograph.api.tests/src/org/eclipse/chronograph/api/test/TestBrickAdapter.java
+++ tests/org.eclipse.chronograph.api.tests/src/org/eclipse/chronograph/api/test/TestBrickAdapter.java
@@ -1,24 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2020 ArSysOp and Others
+ * Copyright (c) 2020 ArSysOp
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Sergei Kovalchuk<sergei.kovalchuk@arsysop.ru> - initial API and implementation
+ *     Sergei Kovalchuk <sergei.kovalchuk@arsysop.ru> - initial API and implementation
  *******************************************************************************/
-package org.eclipse.chronograph.api.brick.test;
+package org.eclipse.chronograph.api.test;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,7 +18,6 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import org.eclipse.chronograph.api.test.TestInputObject;
 import org.eclipse.chronograph.internal.api.Brick;
 import org.eclipse.chronograph.internal.base.BrickImpl;
 
